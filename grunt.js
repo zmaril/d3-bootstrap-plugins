b2822228942104ea3bd8fec9856a464a2f2bc974
Zack Maril <zack@zacharymaril.com>
2012-07-06 23:42:36 -0200

    Grunt. Grunt? Grunt.

--- grunt.js
+++ grunt.js
@@ -1,58 +1,53 @@
 /*global module:false*/
 module.exports = function(grunt) {
+    //Load in the coffee and css grunt machines!
+    grunt.loadNpmTasks('grunt-coffee'); // http://github.com/avalade/grunt-coffee
+    grunt.loadNpmTasks('grunt-css'); // http://github.com/jzaefferer/grunt-css
+    // Project configuration.
+    grunt.initConfig({
+                         pkg: '<json:package.json>',
+                         meta: {
+                             banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+                                 '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                                 '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+                                 '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+                                 ' Licensed <%= pkg.license %> */'
+                         },
+                         coffee:{
+                             coffee:{
+                                 src: ["src/js/plugins.coffee"],
+                                 dest: 'build/js/',
+                                 options:{
+                                     bare: true
+                                 }
+                             }
+                         },                      
+                         concat: {
+                             dist: {
+                                 src: ['build/js/*'],
+                                 dest: 'dist/js/<%= pkg.name %>.js'
+                             }
+                         },                         
+                         min: {
+                             dist: {
+                                 src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+                                 dest: 'dist/js/<%= pkg.name %>.min.js'
+                             }
+                         },
+                         cssmin:{
+                            dist:{
+                                src:["src/css/*.css"],
+                                dest:"dist/css/<%= pkg.name %>.min.css"
+                            }
+                         },
+                         watch: {
+                             files: ["src/*/*"],
+                             tasks: 'coffee concat min cssmin'
+                         }
+                     }
+                    );
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:package.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
-    lint: {
-      files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    concat: {
-      dist: {
-        src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    min: {
-      dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint qunit'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true
-      },
-      globals: {}
-    },
-    uglify: {}
-  });
-
-  // Default task.
-  grunt.registerTask('default', 'lint qunit concat min');
-
+    // Default task.
+    grunt.registerTask('default', 'watch');
+    grunt.registerTask('build','coffee concat min cssmin');
 };
